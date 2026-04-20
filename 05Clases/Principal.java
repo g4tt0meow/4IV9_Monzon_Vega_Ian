26b9eca921303432a0ecd542420087a5e1b21460
g4tt0meow <imonzonv2400@alumno.ipn.mx>
2026-04-19 20:18:40 -0600

    Principal

---
+++ 05Clases/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    //metodo principal
+    public static void main(String[] args){
+        //un ejercicio de figuritas
+        Ejercicios ejecucion = new Ejercicios();
+        ejecucion.menu();
+    }
+}
