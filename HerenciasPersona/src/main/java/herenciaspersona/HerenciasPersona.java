12f85dcfb8af1f0b3a87c7884c0fdf010ac3888e
G4T0MEOW <ianadolfomonzonvega@gmail.com>
2026-05-09 13:44:18 -0600

    Actualizando el git, Arreglos y Herencias

---
+++ HerenciasPersona/src/main/java/herenciaspersona/HerenciasPersona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package herenciaspersona;
+
+/**
+ *
+ * @author ianmo
+ */
+public class HerenciasPersona {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
