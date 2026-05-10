06404b7b0af996e35c985d89cc15dbdeafb3fffc
g4tt0meow <imonzonv2400@alumno.ipn.mx>
2026-05-10 01:44:54 -0600

    Listo las primeras 4 figuras

--- 06Figuras/Calculadora.java
+++ 06Figuras/Calculadora.java
@@ -5,8 +5,8 @@ public class Calculadora {
 
     Scanner sc = new Scanner(System.in);
 
-    double lado, alto, ancho, radio;
-    double diametro = radio / 2;
+    double lado, alto, ancho, diametro, perimetro;
+    double radio = diametro/2;
     double baseMayor, baseMenor;
     char respuesta;
 
@@ -38,7 +38,9 @@ public class Calculadora {
                 case 3:
                     Triangulo();
                     break;
-
+                case 4:
+                    Circulo();
+                    break;
                 default:
                     break;
             }
@@ -114,4 +116,86 @@ public class Calculadora {
         } while (respuesta == 's' || respuesta == 'S');
 
     }
+
+    public void Circulo() {
+        do {
+            System.out.println("Calcular Circulo");
+            System.out.println("Con que quieres calcular tu circulo?");
+            System.out.println("_______________");
+            System.out.println("|1.- Area     |");
+            System.out.println("|2.- Perimetro|");
+            System.out.println("|_____________|");
+            
+            int opcion;
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Con que valor quieres calcular el area?");
+                    System.out.println("_______________");
+                    System.out.println("|1.-Radio     |");
+                    System.out.println("|2.-Diametro  |");
+                    System.out.println("|3.-Perimetro |");
+                    System.out.println("|_____________|");
+
+                    opcion = sc.nextInt();
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Ingresa el radio:");
+                            radio = sc.nextInt();
+                            double area;
+                            area = 3.14 * radio * radio;
+                            System.out.println("El area es de: " + area);
+                            break;
+                        case 2:
+                            System.out.println("Ingresa el diametro:");
+                            diametro = sc.nextDouble();
+                            area = 3.14 * diametro/2 * diametro/2;
+                            System.out.println("El area es de: " + area);
+                            break;
+                        case 3:
+                            System.out.println("Ingresa el perimetro de tu circunferencia");
+                            perimetro = sc.nextDouble();
+                            area = (perimetro * perimetro) / 4 * (3.14);
+                            System.out.println("El area es de: " + area);
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Con que quieres calcular tu perimetro?");
+                    System.out.println("______________");
+                    System.out.println("|1.-Radio    |");
+                    System.out.println("|2.-Diametro |");
+                    System.out.println("|____________|");
+
+                    opcion = sc.nextInt();
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Ingresa el radio:");
+                            radio = sc.nextDouble();
+                            perimetro = 2 * 3.14 * radio;
+                            System.out.println("El perimetro es de: " + perimetro);
+                            break;
+                        case 2:
+                            System.out.println("Ingresa el diametro:");
+                            diametro = sc.nextDouble();
+                            perimetro = 2 * 3.14 * (diametro/2);
+                            System.out.println("El perimetro es de: " + perimetro);
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+                    break;
+                default:
+                    throw new AssertionError();
+            
+        }
+        System.out.println("Quiere repetir? (s/n)");
+        respuesta = sc.next().charAt(0);
+    } while (respuesta == 's' || respuesta == 'S');
+}
 }
